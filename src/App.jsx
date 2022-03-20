52e6a6e4fafa15cbdcead66336a6d3fc23f3f901
dbae82 <dbae82@gmail.com>
2022-03-20 15:17:45 -0500

    edit: adds description to portfolio section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Skills from './components/skills/Skills'
-import Services from './components/services/Services'
+// import Services from './components/services/Services'
 import Projects from './components/projects/Projects'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
@@ -15,7 +15,7 @@ const App = () => {
       <Nav />
       <About />
       <Skills />
-      <Services />
+      {/* <Services /> */}
       <Projects />
       <Contact />
       <Footer />
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -10,6 +10,7 @@ const data = [
     id: 1,
     image: project1,
     title: "Project Sports Tweets",
+    description: "Project Sports Tweets is a full MERN stack application powered by the Twitter API to provide sports fans an up to date feed of Tweets for their favorite teams.",
     github: "https://github.com/dbae82/project-sports-tweets",
     demo: "https://sports-tweets.netlify.app/",
   },
@@ -17,6 +18,7 @@ const data = [
     id: 2,
     image: project2,
     title: "Project Wayfarer",
+    description: "Project wayfarer is a city based blog where users can create blog articles about their travels. This app was made using Django and PostgreSQL.",
     github: "https://github.com/dbae82/project-wayfare",
     demo: "https://bestwayfarer.herokuapp.com/",
   },
@@ -24,6 +26,7 @@ const data = [
     id: 3,
     image: project3,
     title: "Bad Apple Movies",
+    description: "Bad Apple Movies is a full NEM stack web application where users could share opinions on what they think of movies by submitting reviews and holding discussions with other users.",
     github: "https://github.com/dbae82/Project-1-Reverse-Engineer",
     demo: "https://badapplemovies.herokuapp.com/",
   },
@@ -31,6 +34,7 @@ const data = [
     id: 4,
     image: project4,
     title: "Project L.O.A.D.",
+    description: "Life of a Dev is a simple web application where you as a character will need to upkeep various metrics by clicking on buttons that correspond to the colored status bars. Through time, the character will evolve and after 2 evolutions the character will reach retirement.",
     github: "https://github.com/dbae82/project-0-game",
     demo: "https://dbae82.github.io/project-0-game/",
   },
@@ -43,13 +47,16 @@ const Projects = () => {
       <h2>Projects</h2>
 
       <div className="container projects__container">
-        {data.map(({ id, image, title, github, demo }) => {
+        {data.map(({ id, image, title, description, github, demo }) => {
           return (
             <article key={id} className="projects__item">
               <div className="projects__item-image">
                 <img src={image} alt={title} />
               </div>
               <h3>{title}</h3>
+              <div className="projects__item-description">
+                <p>{description}</p>
+              </div>
               <div className="projects__item-cta">
                 <a href={github} target="_blank" className="btn">
                   Github

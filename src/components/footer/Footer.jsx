545e3b4e28de9ad3641b9d5273fc4defa7b0dc6b
dbae82 <dbae82@gmail.com>
2022-03-20 15:53:23 -0500

    build: footer component built and styled

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import { FaLinkedin, FaGithubAlt } from "react-icons/fa";
+import logo from "../../assets/logo-dark.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        <img src={logo} alt="logo" height="70px" />
+      </a>
 
-export default Footer
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/dan-bae/">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/dbae82">
+          <FaGithubAlt />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Dan Bae. All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
